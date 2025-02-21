543908b4bfa58f96275886e3bbb5cefa46888091
Gerardo <g.socias29@gmail.com>
2025-02-21 23:30:58 +0800

    added queue model and departments

--- app/Models/Queue.php
+++ app/Models/Queue.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\{Model, SoftDeletes};
 
 class Queue extends Model
 {
-    //
+  use SoftDeletes;
+
+  protected $fillable = [
+    'department_id',
+    'priority',
+    'number',
+    'status',
+  ];
+  
 }
